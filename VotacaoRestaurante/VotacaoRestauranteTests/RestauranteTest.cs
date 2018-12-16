6cf8d291cdbf68fc7f4e17bad39876d4133fb6be
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-12-16 17:12:07 -0200

    First Commit: Falha do teste para criação do restaurante

---
+++ VotacaoRestaurante/VotacaoRestauranteTests/RestauranteTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VotacaoRestaurante;
+
+namespace VotacaoRestauranteTests
+{
+    [TestClass]
+    public class RestauranteTest
+    {
+        [TestMethod]
+        public void DeveCriarUmNovoRestauranteParaVotacao()
+        {
+            Restaurante restaurante = new Restaurante("Me Gusta");
+        }
+    }
+}
