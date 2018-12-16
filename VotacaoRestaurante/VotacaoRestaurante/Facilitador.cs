252de4fed2f5a7eaca6c28e48a940512a8c8a27c
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-12-16 21:50:38 -0200

    Adicionado teste para cadastrar um novo restaurante para voto (FALHA)

--- VotacaoRestaurante/VotacaoRestaurante/Facilitador.cs
+++ VotacaoRestaurante/VotacaoRestaurante/Facilitador.cs
@@ -8,5 +8,15 @@
         {
             Nome = nomeFacilitador;
         }
+
+        public void AdicionarNovoRestaurante(string meGusta)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int QuantidadeRestaurantesCadastrados()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- VotacaoRestaurante/VotacaoRestauranteTests/FacilitadorTests.cs
+++ VotacaoRestaurante/VotacaoRestauranteTests/FacilitadorTests.cs
@@ -6,14 +6,26 @@ namespace VotacaoRestauranteTests
     [TestClass]
     public class FacilitadorTests
     {
+        private Facilitador facilitador;
+        [TestInitialize]
+        public void DevePrepararOsTestesDoFacilitador()
+        {
+            facilitador = new Facilitador("Hikari Sakamoto");
+        }
         /// <summary>
         /// Deve criar um novo facilitador.
         /// </summary>
         [TestMethod]
         public void DeveCriarUmNovoFacilitador()
         {
-            Facilitador facilitador = new Facilitador("Hikari Sakamoto");
             Assert.AreEqual("Hikari Sakamoto", facilitador.Nome);
+        }
+
+        [TestMethod]
+        public void DeveAdicionarUmNovoRestauranteParaSerVotado()
+        {
+            this.facilitador.AdicionarNovoRestaurante("Me Gusta");
+            Assert.AreEqual(1, this.facilitador.QuantidadeRestaurantesCadastrados());
         }
     }
 }
