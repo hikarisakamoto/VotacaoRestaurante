410c3f8aeece7defe9f12528030ea83342486e2f
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-12-16 21:46:42 -0200

    Alterada a propriedade nome para get only (REFATORACAO)

--- VotacaoRestaurante/VotacaoRestaurante/Facilitador.cs
+++ VotacaoRestaurante/VotacaoRestaurante/Facilitador.cs
@@ -2,7 +2,7 @@
 {
     public class Facilitador
     {
-        public string Nome { get; private set; }
+        public string Nome { get; }
 
         public Facilitador(string nomeFacilitador)
         {
