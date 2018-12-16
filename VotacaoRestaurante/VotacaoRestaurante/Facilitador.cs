9d17ee4a89591a9d7ce8bbb1002cdef680cc5702
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-12-16 21:40:02 -0200

    REFATORAÇÃO COMPLETA DO SISTEMA

--- VotacaoRestaurante/VotacaoRestaurante/Facilitador.cs
+++ VotacaoRestaurante/VotacaoRestaurante/Facilitador.cs
@@ -1,42 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace VotacaoRestaurante
+﻿namespace VotacaoRestaurante
 {
     public class Facilitador
     {
-        public ProfissionalFaminto ProfissionalFacilitador { get; }
-        private List<Restaurante> _restaurantes;
-
-        public Facilitador(ProfissionalFaminto profissional)
-        {
-            ProfissionalFacilitador = profissional;
-            _restaurantes = new List<Restaurante>();
-        }
-
-        public void AdicionarNovoRestauranteParaVoto(Restaurante restaurante)
-        {
-            ValidarRestauranteExisteNoCadastro(restaurante);
-
-            _restaurantes.Add(restaurante);
-        }
-
-        private void ValidarRestauranteExisteNoCadastro(Restaurante restaurante)
-        {
-            if (_restaurantes.Count(restauranteNaLista => restauranteNaLista.Nome.Equals(restaurante.Nome)) > 0)
-                throw new InvalidOperationException();
-        }
-
-        public int QuantidadeRestaurantesCadastrados()
-        {
-            return _restaurantes.Count;
-        }
-
-
-        public bool ReceberVotoParaRestaurante(ProfissionalFaminto profissionalFaminto, Restaurante restaurante)
-        {
-            return true;
-        }
+       
     }
 }
---
+++ VotacaoRestaurante/VotacaoRestauranteTests/FacilitadorTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VotacaoRestaurante;
+
+namespace VotacaoRestauranteTests
+{
+    [TestClass]
+    public class FacilitadorTests
+    {
+       
+    }
+}
